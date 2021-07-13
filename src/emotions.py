d08642c4e72be291dff1c6a027956fa48082c876
Isabelle Bouchard <isabelle.bouchard77@gmail.com>
2021-07-13 14:14:22 -0400

    print complete prediction

--- src/emotions.py
+++ src/emotions.py
@@ -123,9 +123,19 @@ elif mode == "display":
             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            prediction = model.predict(cropped_img).tolist()[0]
+
+            for i, pred in enumerate(prediction):
+                pos = (x , y - 60 - 15 * i)
+
+                if pred > 0.2:
+                    color = (0, 255, 255)
+                else:
+                    color = (255, 255, 255)
+                text = emotion_dict[i]
+                text += ": "
+                text += "{:.2f}".format(pred)
+                cv2.putText(frame, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
         cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
